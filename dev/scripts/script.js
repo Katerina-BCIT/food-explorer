fb4842793ab03707d60e02c01d56159586f4ed1e
DGGeorgiev <dragan.georgiev@yahoo.com>
2020-11-19 10:59:36 -0800

    Change repository

--- dev/scripts/script.js
+++ dev/scripts/script.js
@@ -1,1 +1,9 @@
 // Write your JavaScript here...
+
+const $body = $('body');
+const $btnMainNav = $('#btn-main-nav');
+
+$btnMainNav.click(function(e){
+    e.preventDefault();
+    $body.toggleClass('show-nav');
+});
